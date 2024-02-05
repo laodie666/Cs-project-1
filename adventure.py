1e5be56597b6bfea27820b128b6468ae81da7fa2
laodie666 <mhkjordan@gmail.com>
2024-02-04 22:43:40 -0500

    update game data

--- adventure.py
+++ adventure.py
@@ -104,6 +104,10 @@ def go(d: str, player: Player, world: World):
     elif current_x == 0 and current_y == 3 and w.Lucky_pen_quest.progress < 1:
         print("Grandma is still waiting for you to explain why you are here. ")
 
+
+    elif current_x == 4 and current_y == 2 and world.get_location(current_x, current_y).visited == False:
+        w.T_card_quest.progress += 1
+
     # valid move
     else:
         p.x = current_x
